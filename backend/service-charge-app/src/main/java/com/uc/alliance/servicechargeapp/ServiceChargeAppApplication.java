712f93015ffbb8175d9cc672781e669711081407
StarGaze870 <ljtudtud@gmail.com>
2023-03-19 14:12:17 +0800

    backend test

---
+++ backend/service-charge-app/src/main/java/com/uc/alliance/servicechargeapp/ServiceChargeAppApplication.java
@@ -0,0 +1,13 @@
+package com.uc.alliance.servicechargeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceChargeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceChargeAppApplication.class, args);
+	}
+
+}
---
+++ backend/service-charge-app/src/test/java/com/uc/alliance/servicechargeapp/ServiceChargeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uc.alliance.servicechargeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceChargeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
