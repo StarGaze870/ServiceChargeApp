b5842919226352bd71a39e4e32b3d91ac3a9d471
StarGaze870 <ljtudtud@gmail.com>
2023-03-19 12:57:45 +0800

    Initial commit

---
+++ service_charge_queries.sql
@@ -0,0 +1,26 @@
+use service_charge;
+
+-- TODO: GENERATE DEFAULT VALUES
+
+-- GENERATE ROLES
+-- 1. client - a user who avails of the service
+-- 2. sales - a user who manages sales-related concerns
+-- 3. billing - a user who handles billing and invoicing
+-- 4. collection - a user who manages collections and payments
+-- 5. treasury - a user who oversees financial operations
+-- 6. admin - a user who has full access and control over the system
+
+-- TODO:
+
+-- GENERATE STATUS
+-- 1. pending - a ticket that is waiting to be resolved
+-- 2. closed - a ticket that has been resolved and closed
+
+-- TODO:
+
+-- GENERATE PRIORITIES
+-- 1. high - a ticket that requires immediate attention and resolution
+-- 2. medium - a ticket that needs to be resolved in a reasonable amount of time
+-- 3. low - a ticket that can be addressed at a later time
+
+
